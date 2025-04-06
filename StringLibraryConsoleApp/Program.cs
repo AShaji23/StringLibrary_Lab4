bd431e1e8084d83957abbf3f040607211b9533a9
Ashwin Shaji <ashaji23@my.centennialcollege.ca>
2025-04-06 17:46:52 -0400

    Updated with console App

---
+++ StringLibraryConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using StringLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        string word1 = "Hello";
+        string word2 = "world";
+
+        Console.WriteLine($"{word1} starts with uppercase? {word1.StartsWithUpperCase()}");
+        Console.WriteLine($"{word2} starts with uppercase? {word2.StartsWithUpperCase()}");
+    }
+}
